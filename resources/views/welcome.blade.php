7fdcb6d60657c19c2811bf364b48427cffce9b91
NanMoKham <nanmokham948@gmail.com>
2022-04-06 16:47:32 +0630

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <div class="container">
+        <h2>Handsontable Grid</h2>
+        <div id="example"></div>
+    </div>
+</body>
+<!-- js files -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
+<script src="/js/handsontable.js"></script>
+<script src="/js/handsontable.full.min.js"></script>
+<script src="/js/grid.js"></script>
+<!-- css files -->
+<link type="text/css" rel="stylesheet" href="/css/handsontable.css">
+<link type="text/css" rel="stylesheet" href="/css/handsontable.full.min.css" />
+
+</html>
